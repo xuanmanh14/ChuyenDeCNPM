4c1ee7ac0f89cb14718fb14a974961f9e016b970
xuanmanh14 <97585812+xuanmanh14@users.noreply.github.com>
2023-09-21 11:37:31 +0700

    Bai thuc hanh buoi 4

---
+++ index.js
@@ -0,0 +1,64 @@
+// console.log("Hello World");
+
+// let a = 0;
+
+// const B = 1;
+
+// // a = "Hello World";
+// console.log(a + B, a - B, a * B, a / B);
+
+// const myObject = {
+//   id: "1232131",
+//   name: "Hoc Tran",
+// };
+
+// const name = "Hoc Tran";
+
+// //Array
+// const myArray = [];
+// myArray.push("hoctran");
+// myArray.push(1);
+
+// let a = 1;
+
+// let b = a;
+
+// a = 2;
+
+// console.log(b); //1
+
+// let aObject = {
+// //   a: 1,
+// // };
+
+// // let bObject = aObject;
+
+// // aObject.a = 2;
+
+// // console.log(bObject.a); // ?
+
+// let a = 1;
+// let b = 2;
+// if (a == b) {
+//   console.log(true);
+// } else if (a > b) {
+//   console.log("a lon hon b");
+// } else {
+//   console.log(false);
+// }
+
+// for (let i = 0; i < b; i++) {
+//   console.log(i);
+// }
+
+// let myArray = [1, 2, 3, 4];
+// myArray.forEach((element) => {
+//   console.log(element);
+// });
+
+// function tinhTong(a, b) {
+//   return a + b;
+// }
+
+let A = [1, 1, 2, 3, 4]; // true
+let B = [1, 2, 3, 4]; // false
